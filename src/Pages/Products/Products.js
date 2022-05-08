7218df54ede423dc0e7b633879a0d31c1666f0eb
siam <you@example.com>
2022-05-08 20:29:57 +0530

    added few pages

---
+++ src/Pages/Products/Products.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./Products.css";
+import Product from "../Product/Product";
+const Products = () => {
+  //state
+  const [products, setProducts] = useState([]);
+
+  //hook
+  useEffect(() => {
+    const url = `https://farns-warehouse.herokuapp.com/products`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+  }, []);
+
+  const p = products.slice(0,6);
+  return (
+    <div>
+      <div className="container-fluid" id="products">
+        <h1 className="text-center my-3">Featured Products</h1>
+        <div className="container">
+          <div className="row row-cols-lg-3">
+            {p.map((product) => (
+              <Product key={product._id} products={product}></Product>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
